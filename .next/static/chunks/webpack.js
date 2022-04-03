5a81eca3c76ce55d96fd99addcf3a58b596c7329
glinckyIs <glinckyweb@gmail.com>
2022-04-03 19:05:28 +0300

    fixed header link line spacing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b964a53e86f191c9"; }
+/******/ 		__webpack_require__.h = function() { return "21ffab5a5cad6959"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
