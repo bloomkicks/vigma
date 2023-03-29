72f2a3082400da7f82981b856aa50053e0a20013
bloomkicks <glinckyweb@gmail.com>
2023-03-29 19:46:36 +0300

    optimized images and twisted styles a bit on heading margins and Catalog padding

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 
 module.exports = {
@@ -9,7 +10,11 @@ module.exports = {
   webpack(config, options) {
     config.optimization = {
       minimize: true,
-      minimizer: [new HtmlMinimizerPlugin(), new CssMinimizerPlugin()],
+      minimizer: [
+        new CssMinimizerPlugin(),
+        new HtmlMinimizerPlugin(),
+        new TerserPlugin(),
+      ],
     };
     return config;
   },
